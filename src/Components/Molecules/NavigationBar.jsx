27fb2f0ab1866666e3f4e02f3424797dafdfa736
Gulammustufa Ansari <gmansari0064@gmail.com>
2024-02-28 22:55:06 +0530

    first commit

---
+++ src/Components/Molecules/NavigationBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavigationBar() {
+  return (
+    <div className={`bg-primaryColor h-[7vh] w-screen flex items-center pl-5`}>
+      <p className={`text-white text-lg`}>mubiCARE</p>
+    </div>
+  );
+}
+
+export default NavigationBar;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {colors: {
+      primaryColor: '#035055',
+      secondaryColor: '#118A92',
+      tertiaryColor: '#1B998B'
+    },},
+  },
+  plugins: [],
+}
+
