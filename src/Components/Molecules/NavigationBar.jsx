55dab881ecfdd6e0ea07fa046a57b91d359f60d3
mustufa <gmansari0064@gmail.com>
2024-03-05 20:42:43 +0530

    Slect type done

--- src/Components/Molecules/NavigationBar.jsx
+++ src/Components/Molecules/NavigationBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NavigationBar() {
   return (
     <div className={`bg-primaryColor h-[7vh] w-screen flex items-center pl-5`}>
-      <p className={`text-white text-lg`}>mubiCARE</p>
+      <p className={`text-white text-lg`}>mubiCARE2</p>
     </div>
   );
 }
